44aa64d291c4eac0547950e45b1c1c6cccdb93d9
Max Hausch <github@hausch.xyz>
2024-07-08 12:05:36 +0200

    feat: Substitute hardcoded timespan and render it via jinja

--- vary_my_params/prepare_simulation/pflotran/pflotran_in_renderer.py
+++ vary_my_params/prepare_simulation/pflotran/pflotran_in_renderer.py
@@ -2,7 +2,9 @@ import jinja2
 
 
 def render():
+    data = {"time": {"final_time": 27.5}}
+
     env = jinja2.Environment(loader=jinja2.PackageLoader("vary_my_params.prepare_simulation.pflotran"))
 
     template = env.get_template("pflotran.in.j2")
-    print(template.render())
+    print(template.render(data))
