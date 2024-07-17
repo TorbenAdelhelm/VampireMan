0495f4ac55700a71cbfdaa4412f10badcf84672c
Max Hausch <github@hausch.xyz>
2024-07-17 13:00:51 +0200

    feat(prepare_simulation/pflotran_in_renderer): Allow renderer to be called directly

--- vary_my_params/prepare_simulation/pflotran/pflotran_in_renderer.py
+++ vary_my_params/prepare_simulation/pflotran/pflotran_in_renderer.py
@@ -1,10 +1,16 @@
+import pathlib
+
 import jinja2
 
 
 def render():
     data = {"time": {"final_time": 27.5}}
 
-    env = jinja2.Environment(loader=jinja2.PackageLoader("vary_my_params.prepare_simulation.pflotran"))
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader(pathlib.Path(__file__).parent / "templates"))
 
     template = env.get_template("pflotran.in.j2")
     print(template.render(data))
+
+
+if __name__ == "__main__":
+    render()
