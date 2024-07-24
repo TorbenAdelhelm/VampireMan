df8a793cde81763834f4f4927dc5919d84324085
Max Hausch <github@hausch.xyz>
2024-07-24 21:21:19 +0200

    feat: Modify data attribute in the vary_params stage instead of the render stage

--- vary_my_params/prepare_simulation/pflotran/pflotran_in_renderer.py
+++ vary_my_params/prepare_simulation/pflotran/pflotran_in_renderer.py
@@ -8,21 +8,12 @@ from .pflotran_generate_mesh import write_mesh_and_border_files
 
 
 def render(config: Config):
-    data = {"time": {"final_time": 27.5}}
-
-    permeability = config.data.get("permeability")
-    assert permeability is not None, "Permeability is not set, cannot continue"
-    data["permeability"] = permeability.value
-
-    data["number_cells"] = config.data.get("number_cells").value
-    data["cell_resolution"] = config.data.get("cell_resolution").value
-
-    write_mesh_and_border_files(data, config.general.output_directory)
+    write_mesh_and_border_files(config.data, config.general.output_directory)
     logging.debug("Rendered {north,east,south,west}.ex")
 
     env = jinja2.Environment(loader=jinja2.FileSystemLoader(pathlib.Path(__file__).parent / "templates"))
     template = env.get_template("pflotran.in.j2")
 
     with open(f"{config.general.output_directory}/pflotran.in", "w") as file:
-        file.write(template.render(data))
+        file.write(template.render(config.data))
         logging.debug("Rendered pflotran.in")
--- vary_my_params/vary_params/pflotran.py
+++ vary_my_params/vary_params/pflotran.py
@@ -2,5 +2,9 @@ from ..config import Config
 
 
 def vary_params(config: Config) -> Config:
-    config.data = config.parameters
+    config.data["permeability"] = config.parameters.get("permeability").value
+    config.data["number_cells"] = config.parameters.get("number_cells").value
+    config.data["cell_resolution"] = config.parameters.get("cell_resolution").value
+    config.data["time"] = {"final_time": 27.5}
+
     return config
