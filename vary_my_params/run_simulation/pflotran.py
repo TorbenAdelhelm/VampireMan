e64cbc75e34e7117cd4d61fb82b392cf44e2b3d6
Max Hausch <github@hausch.xyz>
2024-08-18 12:41:56 +0200

    fix: chdir to original_dir AFTER running the simulation, as otherwise we reside in the sim dir

--- vary_my_params/run_simulation/pflotran.py
+++ vary_my_params/run_simulation/pflotran.py
@@ -8,7 +8,7 @@ def run_simulation(config: Config):
     original_dir = os.getcwd()
 
     for index in range(config.general.number_datapoints):
+        os.chdir(config.general.output_directory / f"datapoint-{index}")
+        subprocess.run(["mpirun", "-n", "1", "pflotran"], check=True, close_fds=True)
         # always go back to the original_dir as we use relative paths
         os.chdir(original_dir)
-        os.chdir(config.general.output_directory / f"datapoint-{index}")
-        subprocess.run(["mpirun", "-n", "1", "pflotran"])
