070e5ba2b95b7b9fadb465d0e297ce531702fe2a
Max Hausch <github@hausch.xyz>
2024-08-21 10:06:43 +0200

    feat: Allow heatpumps to be varied

--- vary_my_params/vary_params/pflotran.py
+++ vary_my_params/vary_params/pflotran.py
@@ -1,4 +1,7 @@
+import numpy as np
+
 from ..config import Config, Data, Datapoint, DataType, HeatPump, Parameter, ParameterHeatPump, Vary
+from ..utils import random_float
 from .vary_perlin import create_const_field, create_vary_field
 
 
@@ -9,10 +12,21 @@ def copy_parameter(parameter: Parameter) -> Data:
 
 def vary_heatpump(config: Config, parameter: ParameterHeatPump) -> Data:
     resolution = config.general.cell_resolution.value
+    number_cells = config.general.number_cells.value
+
     hp = parameter.value
     result_location = [0.0, 0.0, 0.0]
-    for i in range(3):
-        result_location[i] = hp.location[i] * resolution[i]
+
+    # XXX is this needed?
+    match parameter.vary:
+        case Vary.CONST:
+            for i in range(3):
+                result_location[i] = random_float(config) * resolution[i] * (number_cells[i] - 1 + 0.5)
+        case Vary.NONE:
+            for i in range(3):
+                result_location[i] = (hp.location[i] - 1 + 0.5) * resolution[i]
+        case _:
+            raise NotImplementedError()
 
     return Data(parameter.name, parameter.data_type, HeatPump(result_location, hp.injection_temp, hp.injection_rate))
 
@@ -41,6 +55,13 @@ def vary_params(config: Config) -> Config:
                         case _:
                             raise NotImplementedError()
                     data[parameter.name] = Data(parameter.name, parameter.data_type, field)
+                # TODO make this less copy paste
+                case Vary.CONST:
+                    match parameter.data_type:
+                        case DataType.HEATPUMP:
+                            data[parameter.name] = vary_heatpump(config, parameter)  # type: ignore
+                        case _:
+                            raise NotImplementedError()
                 case _:
                     raise ValueError()
 
