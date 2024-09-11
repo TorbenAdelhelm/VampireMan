d064a4cd4aadb79974f56d5b22d8bce4d43355f7
Max Hausch <github@hausch.xyz>
2024-09-11 14:43:32 +0200

    feat: Ask user to skip the simulation if pflotran.(h5|out) are present

--- vary_my_params/run_simulation/pflotran.py
+++ vary_my_params/run_simulation/pflotran.py
@@ -1,14 +1,22 @@
+import logging
 import os
 import subprocess
 
 from ..config import Config
+from ..utils import get_answer
 
 
 def run_simulation(config: Config):
     original_dir = os.getcwd()
 
     for index in range(config.general.number_datapoints):
-        os.chdir(config.general.output_directory / f"datapoint-{index}")
+        datapoint_path = config.general.output_directory / f"datapoint-{index}"
+        os.chdir(datapoint_path)
+        if os.path.exists("pflotran.out") and os.path.exists("pflotran.h5"):
+            logging.warn(f"pflotran.out and pflotran.h5 files present in {datapoint_path}")
+            if get_answer(config, "Looks like the simulation already ran, skip simulation?"):
+                os.chdir(original_dir)
+                continue
         subprocess.run(["mpirun", "-n", "1", "pflotran"], check=True, close_fds=True)
         # always go back to the original_dir as we use relative paths
         os.chdir(original_dir)
