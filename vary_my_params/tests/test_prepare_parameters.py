e4e904397b2b9aff04047762094a85941721a8ff
Max Hausch <github@hausch.xyz>
2024-11-06 19:22:37 +0100

    feat(tests): Adjust tests to use/test TimeBasedValue in HeatPumps

--- vary_my_params/tests/test_prepare_parameters.py
+++ vary_my_params/tests/test_prepare_parameters.py
@@ -1,6 +1,6 @@
 import pytest
 
-from vary_my_params.config import Config, HeatPump, HeatPumps, Parameter, Vary
+from vary_my_params.config import Config, HeatPump, HeatPumps, Parameter, ParameterValueMinMax, TimeBasedValue, Vary
 from vary_my_params.pipeline import prepare_parameters
 
 
@@ -41,17 +41,24 @@ def test_prepare_heatpump_generation():
             vary=Vary.FIXED,
             value=HeatPumps(
                 number=10,
-                injection_temp_min=1,
-                injection_temp_max=2,
-                injection_rate_min=2,
-                injection_rate_max=2,
+                injection_temp=ParameterValueMinMax(min=14, max=18),
+                injection_rate=TimeBasedValue(
+                    time_unit="year",
+                    values={
+                        0: ParameterValueMinMax(min=0, max=0.002),
+                        1: 0,
+                        2: ParameterValueMinMax(min=0.00024, max=0.002),
+                    },
+                ),
             ),
         )
     }
     config = prepare_parameters(config)
     assert len(config.heatpump_parameters) == 10
     assert config.heatpump_parameters.get("hps_0").value.location == [102.5, 172.5, 2.5]
-    assert config.heatpump_parameters.get("hps_9").value.location == [142.5, 147.5, 2.5]
+    assert config.heatpump_parameters.get("hps_9").value.location == [52.5, 97.5, 2.5]
+    assert config.heatpump_parameters.get("hps_9").value.injection_temp.values[0] == 14.814702918850823
+    assert config.heatpump_parameters.get("hps_9").value.injection_rate.values[1] == 0
 
 
 def test_prepare_heatpump_name_clash():
@@ -66,7 +73,9 @@ def test_prepare_heatpump_name_clash():
             name="hp",
             vary=Vary.FIXED,
             value=HeatPumps(
-                number=1, injection_temp_min=1, injection_temp_max=2, injection_rate_min=1, injection_rate_max=2
+                number=1,
+                injection_temp=ParameterValueMinMax(min=1, max=2),
+                injection_rate=ParameterValueMinMax(min=1, max=2),
             ),
         ),
     }
