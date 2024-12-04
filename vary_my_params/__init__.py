d1a952da3966bccaa307c52a667b56ae299051b8
Max Hausch <github@hausch.xyz>
2024-12-04 13:43:04 +0100

    doc(vary_my_params): Re-expose README as __doc__ for the module

--- vary_my_params/__init__.py
+++ vary_my_params/__init__.py
@@ -1,1 +1,8 @@
+import pathlib
 
+# Uses the README.md in the project root as
+try:
+    with open(pathlib.Path(__file__).parent.parent.resolve() / "README.md", encoding="utf8") as readme:
+        __doc__ = "".join(readme.readlines())
+except Exception:
+    __doc__ = "Vary parameters in a structured, declarative, reproducible way."
