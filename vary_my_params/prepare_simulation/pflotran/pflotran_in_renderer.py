b7d53c25ac782a9412e8b6aab7b00879aebff56d
Max Hausch <github@hausch.xyz>
2024-12-04 11:24:20 +0100

    refactor: Change order of parameter rendering

--- vary_my_params/prepare_simulation/pflotran/pflotran_in_renderer.py
+++ vary_my_params/prepare_simulation/pflotran/pflotran_in_renderer.py
@@ -24,20 +24,13 @@ def render(config: Config):
 
     for index, datapoint in enumerate(config.datapoints):
         datapoint_dir = config.general.output_directory / f"datapoint-{index}"
+
         # Ensure hydraulic_head is x, y, z
         hydraulic_head = datapoint.data["hydraulic_head"]
         if isinstance(hydraulic_head.value, float):
             hydraulic_head.value = ValueXYZ(x=0, y=hydraulic_head.value, z=0)
 
-        values = datapoint.data
-        heatpumps = [{name: d.value} for name, d in datapoint.data.items() if isinstance(d.value, HeatPump)]
-        values["heatpumps"] = heatpumps  # type: ignore
-        values["time_to_simulate"] = config.general.time_to_simulate  # type: ignore
-
-        with open(f"{datapoint_dir}/pflotran.in", "w") as file:
-            file.write(template.render(values))
-            logging.debug("Rendered pflotran-%s.in", index)
-
+        # Handle permeability
         permeability = datapoint.data["permeability"]
 
         # Is the permeability already a 3d field? If not, create one
@@ -50,3 +43,13 @@ def render(config: Config):
             permeability.value,
             permeability.name,
         )
+
+        heatpumps = [{name: d.value} for name, d in datapoint.data.items() if isinstance(d.value, HeatPump)]
+
+        values = datapoint.data
+        values["heatpumps"] = heatpumps  # type: ignore
+        values["time_to_simulate"] = config.general.time_to_simulate  # type: ignore
+
+        with open(f"{datapoint_dir}/pflotran.in", "w") as file:
+            file.write(template.render(values))
+            logging.debug("Rendered pflotran-%s.in", index)
