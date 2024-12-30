17caa07b60df0fe3a36b72489c7a60271a61529f
Max Hausch <github@hausch.xyz>
2024-12-30 16:49:00 +0100

    feat(tests): Add test to go over all settings up till render_stage

---
+++ vary_my_params/tests/test_all_settings.py
@@ -0,0 +1,30 @@
+from argparse import Namespace
+from pathlib import Path
+
+from vary_my_params.data_structures import State
+from vary_my_params.loading_stage.utils import loading_stage
+from vary_my_params.pipeline import preparation_stage, render_stage, variation_stage
+from vary_my_params.utils import create_dataset_and_datapoint_dirs, write_data_to_verified_json_file
+from vary_my_params.validation_stage.utils import validation_stage
+
+
+def test_all_settings():
+    pathlist = Path("./settings/").glob("*.yaml")
+    for setting in pathlist:
+        try:
+            state = State.from_yaml(setting)
+            state = loading_stage(
+                Namespace(settings_file=setting, numerical_solver="pflotran", non_interactive=True, log_level="INFO")
+            )
+            if state.general.number_cells[1] > 1000:
+                # Skipping as large settings take ages
+                continue
+            state = preparation_stage(state)
+            state = validation_stage(state)
+            create_dataset_and_datapoint_dirs(state)
+            write_data_to_verified_json_file(state, state.general.output_directory / "state.json", state)
+            state = variation_stage(state)
+            render_stage(state)
+        except Exception as e:
+            print(f"Error in {setting}")
+            raise e
