93a778b85d5e34669899fd7729becffc7cc3dccd
Max Hausch <github@hausch.xyz>
2024-12-31 14:50:45 +0100

    feat: warn -> warning

--- vary_my_params/pflotran/simulation_stage.py
+++ vary_my_params/pflotran/simulation_stage.py
@@ -18,7 +18,7 @@ def simulation_stage(state: State):
         datapoint_path = state.general.output_directory / f"datapoint-{index}"
         os.chdir(datapoint_path)
         if os.path.isfile("pflotran.out") and os.path.isfile("pflotran.h5"):
-            logging.warn(f"pflotran.out and pflotran.h5 files present in {datapoint_path}")
+            logging.warning(f"pflotran.out and pflotran.h5 files present in {datapoint_path}")
             if not get_answer(state, "Looks like the simulation already ran, run simulation again?"):
                 os.chdir(original_dir)
                 continue
