2b3b16ae863389546e8d092ee72462d653b420f1
Max Hausch <github@hausch.xyz>
2025-01-13 11:40:55 +0100

    feat(tests): Add simulation_stage test

---
+++ vary_my_params/tests/test_simulation.py
@@ -0,0 +1,36 @@
+import subprocess
+from unittest.mock import patch
+
+from vary_my_params.data_structures import State
+from vary_my_params.pipeline import preparation_stage, render_stage, simulation_stage, validation_stage, variation_stage
+
+
+def mock_pflotran_call(*args, **kwargs):
+    return subprocess.CompletedProcess(args=args, returncode=0, stdout="Done", stderr="")
+
+
+@patch("subprocess.run", side_effect=mock_pflotran_call)
+def test_simulation(mock_run):
+    state = State()
+    state.general.interactive = False
+
+    state.general.number_datapoints = 5
+    state.general.mpirun = True
+    state.general.mpirun_procs = 1
+    state.general.mute_simulation_output = False
+    state = preparation_stage(state)
+    simulation_stage(state)
+    assert mock_run.call_count == 5
+    mock_run.assert_called_with(["mpirun", "-n", "1", "--", "pflotran"], check=True, close_fds=True)
+
+    state.general.mpirun = False
+    state = preparation_stage(state)
+    simulation_stage(state)
+    mock_run.assert_called_with(["pflotran"], check=True, close_fds=True)
+
+    state.general.mpirun = True
+    state.general.mpirun_procs = None
+    state.general.mute_simulation_output = True
+    state = preparation_stage(state)
+    simulation_stage(state)
+    mock_run.assert_called_with(["mpirun", "--", "pflotran", "-screen_output", "off"], check=True, close_fds=True)
