b98d0040f19a40a6099b07421ff605063cb3ebbc
Max Hausch <github@hausch.xyz>
2025-01-22 20:53:32 +0100

    doc: Document loading_stage

--- vampireman/loading_stage/loading_stage.py
+++ vampireman/loading_stage/loading_stage.py
@@ -1,3 +1,12 @@
+"""
+The loading stage takes care of initializing the `vampireman.data_structures.State` object which is used throughout the
+pipeline run. First, a `vampireman.data_structures.State` object is created with all its default values in place. Then,
+if the user provided a settings file via the `--settings-file` command line parameter, the given yaml file is read and
+another `vampireman.data_structures.State` object with the values from the yaml is instantiated. Afterwards, the first
+object is overridden with the second one to keep the default settings without the user having to explicitly specify
+everything.
+"""
+
 import argparse
 import logging
 
@@ -5,7 +14,8 @@ from ..data_structures import State
 
 
 def loading_stage(arguments: argparse.Namespace) -> State:
-    run_state = State()  # pyright: ignore
+    """Run the stage."""
+    run_state = State()
     logging.debug("Default state is %s", run_state)
 
     # Load settings from file if provided
